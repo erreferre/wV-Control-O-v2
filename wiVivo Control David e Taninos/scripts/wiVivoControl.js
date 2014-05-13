631b50af7c0118345000258b6627169245d15710
aerowi desarrollo <desarrollo@aerowi.es>
2014-05-13 03:51:09 -0500

    Changes made by aerowi desarrollo (desarrollo@aerowi.es).

--- wiVivo Control David e Taninos/scripts/wiVivoControl.js
+++ wiVivo Control David e Taninos/scripts/wiVivoControl.js
@@ -7,16 +7,11 @@ var servidor_wivivo = 'http://srv001.liveshowsync.local';
 //var servidor_wivivo = 'http://192.168.10.155';
 var webservice_wivivo = servidor_wivivo + '/liveshowsync/';
 
-//var servidor_comandos = webservice_wivivo + 'comandos.php'; 
 var servidor_color1 = webservice_wivivo + 'actualiza_color1.php';
 var servidor_color2 = webservice_wivivo + 'actualiza_color2.php';
 var servidor_intermitencia = webservice_wivivo + 'actualiza_intermitencia.php';
-//var servidor_activa_loto = webservice_wivivo + 'activa_loto.php';
-//var servidor_desactiva_loto = webservice_wivivo + 'desactiva_loto.php';
 var servidor_activa_guapo = webservice_wivivo + 'activa_guapo.php';
 var servidor_desactiva_guapo = webservice_wivivo + 'desactiva_guapo.php';
-var servidor_activa_pedo = webservice_wivivo + 'activa_pedo.php';
-var servidor_desactiva_pedo = webservice_wivivo + 'desactiva_pedo.php';
 var servidor_activa_aplauso = webservice_wivivo + 'activa_aplauso.php';
 var servidor_desactiva_aplauso = webservice_wivivo + 'desactiva_aplauso.php';
 var servidor_activa_selfie1 = webservice_wivivo + 'activa_selfie1.php';
@@ -32,7 +27,6 @@ var servidor_activa_show = webservice_wivivo + 'activa_show.php';
 var servidor_activa_showempezado = webservice_wivivo + 'activa_showempezado.php';
 var servidor_desactiva_show = webservice_wivivo + 'desactiva_show.php';
 var servidor_desactiva_showempezado = webservice_wivivo + 'desactiva_showempezado.php';
-//var servidor_actualiza_tiempo = webservice_wivivo + 'actualiza_tiempo.php';
 var servidor_activa_alertas = webservice_wivivo + 'activa_alertas.php';
 var servidor_desactiva_alertas = webservice_wivivo + 'desactiva_alertas.php';
 
@@ -40,16 +34,13 @@ var nombreFoto = null;
 
 //temporizadores
 var activaShowEmpezadosetTimeout = null;
-//var desactivaLotosetTimeout = null;
 var desactivaGuaposetTimeout = null;
 var desactivaAplausosetTimeout = null;
-var desactivaPedosetTimeout = null;
 
 // PhoneGap is ready
 function onDeviceReady() {
-    //navigator.splashscreen.hide();
     window.plugins.powerManagement.acquire();
-    //PONER menubutton CUANDO SEA LA VERSION RELEASE
+    //CAMBIAR CUANDO SEA LA VERSION RELEASE
     document.addEventListener("menubutton", exitAppPopup, false);
     //document.addEventListener("backbutton", atrasApp, false);
     document.addEventListener("backbutton", exitAppPopup, false);
@@ -59,7 +50,7 @@ function onDeviceReady() {
 function leeConfiguracion() {
     $.getJSON(servidor_lee)
     .fail(function(jqxhr, textStatus, error){
-    	navigator.notification.alert("Proba de novo, ou sae da App (pulsando o botón menú do teu móbil), conéctate á WiFi e volve a lanza-la App",function(){},"ERRO DE COMUNICACION","OK");
+    	navigator.notification.alert("Proba de novo, ou cerra a App, conéctate á WiFi e volve a lanza-la App",function(){},"ERRO DE COMUNICACION","OK");
     });
 }
 
@@ -75,24 +66,6 @@ function falloConexion(){
 }
 
 //COLORES
-//function ponColor(color,i){
-//    var tmp1="000000";
-//    var tmp2 = i;
-//    alert(color);
-//    switch (color){
-//        case amarillo: tmp1="ffcc00";break;
-//        case azul: tmp1="00ffff";break;
-//        case fucsia: tmp1="8e149a";break;
-//        case naranja: tmp1="f77331";break;
-//        case rojo: tmp1="ff0000";break;
-//        case verde: tmp1="40ff00";break;
-//        case blanco: tmp1="ffffff";break;
-//        case negro: tmp1="000000";
-//    }
-//    $.get(servidor_comandos, {columna:tmp2,valor:tmp1})
-//    	.done(function(){alertaColor(color,i);})
-//    	.fail(function(){falloConexion();});
-//}
 function ponColor1Rojo(){
     $.get(servidor_color1, {color:"ff0000"})
     	.done(function(){alertaColor('ROJO',1);})
@@ -131,15 +104,16 @@ function intermitenciaNula(){
     	.fail(function(){falloConexion();});
 }
 
-//function intermitenciaBaja(){
-//    $.get(servidor_intermitencia, {intermitencia:"1"})
-//    	.done(function(){alertaComando("BAJA","INTERMITENCIA");})
-//    	.fail(function(){falloConexion();});
-//}
-
 function intermitenciaAlta(){
     $.get(servidor_intermitencia, {intermitencia:"2"})
     	.done(function(){alertaComando("ALTA","INTERMITENCIA");})
+    	.fail(function(){falloConexion();});
+}
+
+//BENGALA
+function activaBengala(){
+    $.get(servidor_intermitencia, {intermitencia:"3"})
+    	.done(function(){alertaComando("ACTIVADA","BENGALA");})
     	.fail(function(){falloConexion();});
 }
 
@@ -195,23 +169,6 @@ function desactivaShow(){
     return false;
 }
 
-//LOTO
-//function activaLoto(){
-//    $.get(servidor_activa_loto)
-//    	.done(function(){alertaComando("ACTIVADA","LOTO");})
-//    	.fail(function(){falloConexion();});
-//    desactivaLotosetTimeout = setTimeout(desactivaLoto,300000);
-//}
-//function desactivaLoto(){
-//    $.get(servidor_desact_loto)
-//    	.done(function(){alertaComando("DESACTIVADA","LOTO");})
-//    	.fail(function(){falloConexion();});
-//	if (desactivaLotosetTimeout !== null) {
-//        clearTimeout(desactivaLotosetTimeout);
-//        desactivaLotosetTimeout = null;
-//    }
-//}
-
 function activaGuapo(){
     $.get(servidor_activa_guapo)
     .done(function(){
@@ -252,26 +209,6 @@ function desactivaAplauso(){
     .fail(function(){falloConexion();});
 }
 
-function activaPedo(){
-    $.get(servidor_activa_pedo)
-    .done(function(){
-        alertaComando("ACTIVADO","PEDO");
-        desactivaPedosetTimeout = setTimeout(desactivaPedo,120000);
-	})
-    .fail(function(){falloConexion();});
-}
-function desactivaPedo(){
-    $.get(servidor_desactiva_pedo)
-    .done(function(){
-        if (desactivaPedosetTimeout !== null) {
-        	clearTimeout(desactivaPedosetTimeout);
-        	desactivaPedosetTimeout = null;
-    	}
-        alertaComando("DESACTIVADO","PEDO");
-    })
-    .fail(function(){falloConexion();});
-}
-
 function activaSelfie1(){
     $.get(servidor_activa_selfie1)
     .done(function(){alertaComando("ACTIVADO","SELFIE1");})
@@ -294,127 +231,7 @@ function desactivaSelfie2(){
     .fail(function(){falloConexion();});
 }
 
-
 //ADMIN
-//function actualizaTGlobal(){
-//	var form = $("#ft_global");   
-//    var tiempo = $("#t_global", form).val();
-//    tiempo = tiempo + '000';
-//    var col = 'startConsultaServidorsetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Global: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function defectoTGlobal(){
-//    var tiempo = 10000;
-//    var col = 'startConsultaServidorsetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Global: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function actualizaTSelfie(){
-//    var form = $("#ft_global");   
-//    var tiempo = $("#t_global", form).val();
-//    tiempo = tiempo + '000';    
-//    var col = 'startSelfiesetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Selfie: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function defectoTSelfie(){
-//    var tiempo = 20000;
-//    var col = 'startSelfiesetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Selfie: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function actualizaTLoto(){
-//    var form = $("#ft_global");   
-//    var tiempo = $("#t_global", form).val();
-//    tiempo = tiempo + '000';
-//    var col = 'startLotosetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Loto: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function defectoTLoto(){
-//    var tiempo = 20000;
-//    var col = 'startLotosetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Loto: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function actualizaTColorines(){
-//    var form = $("#ft_global");   
-//    var tiempo = $("#t_global", form).val();
-//    tiempo = tiempo + '000';
-//    var col = 'startColorinessetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Colorines: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function defectoTColorines(){
-//    var tiempo = 20000;
-//    var col = 'startColorinessetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Colorines: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function actualizaTIntermitencia(){
-//    var form = $("#ft_global");   
-//    var tiempo = $("#t_global", form).val();
-//    var col = 'startIntermitenciasetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Intermitencia: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
-//function defectoTIntermitencia(){
-//    var tiempo = 200;
-//    var col = 'startIntermitenciasetTimeout';
-//    $.get(servidor_actualiza_tiempo, {columna:col,valor:tiempo})
-//    	.done(function(){
-//            alertaComando("T. Loto: "+tiempo,"TEMPORIZADOR");
-//        })
-//    	.fail(function(){
-//            falloConexion();
-//        });
-//}
 function activaAlertas(){
     $.get(servidor_activa_alertas)
     	.done(function(){alertaComando("ACTIVADAS","ALERTAS");})
@@ -426,10 +243,8 @@ function desactivaAlertas(){
     	.fail(function(){falloConexion();});    
 }
 
-
 //Fotos con "capture"
 function captureImage() {
-    // Launch device camera application, 
     navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 1});
 }
 
@@ -451,7 +266,6 @@ function captureSuccess(mediaFiles) {
 // Called if something bad happens.
 function captureError(error) {
     navigator.notification.alert("Volve a saca-la foto", function(){},"ERRO EN CAPTURA", "OK");
-    //navigator.notification.alert(msg, null, 'Uh oh!');
 }
 
 //SUBE FOTO
@@ -473,11 +287,6 @@ function uploadPhoto(mediaFile) {
 }
 
 function uploadSuccess(r) {
-    //console.log("Code = " + r.responseCode);
-    //console.log("Response = " + r.response);
-    //console.log("Sent = " + r.bytesSent);
-	//rutasubida.innerHTML = '<p>foto subida OK!<br/>' + r.responseCode + '<br/>' + r.response + '<br/>' + r.bytesSent + '</p>'
-    //comprimimos foto subida
     $.get(servidor_thumb, {imagen:nombreFoto})
     .done(function(){
 	    var rutasubida2 = document.getElementById("rutasubida");
@@ -489,10 +298,7 @@ function uploadSuccess(r) {
 }
 
 function uploadError(error) {
-    //alert("Saca outra foto. Houbo un erro: Code = " + error.code + ", source = " + error.source + ", target = " + error.target);
     navigator.notification.alert("Saca a foto de novo. Houbo un erro",function(){}, "ERRO NA SUBIDA", "OK");
-    //console.log("upload error source " + error.source);
-    //console.log("upload error target " + error.target);
 }
 
 //nombre de imagen aleatorio
